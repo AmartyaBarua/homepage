53d09744c340b7a197689ee71ef8c4bb53dddd35
AmartyaBarua <barua.amartya@gmail.com>
2020-05-24 18:06:52 -0400

    Updates

---
+++ precache-manifest.4c2355c55677095fec7ce0a9056d210e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef80e434b84f2efe552d63aa7b9d2ab",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "3b2e5f869e394ebecc9c",
+    "url": "/homepage/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "23dee8739d9fb133ae1c",
+    "url": "/homepage/static/js/2.abe03446.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homepage/static/js/2.abe03446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b2e5f869e394ebecc9c",
+    "url": "/homepage/static/js/main.f1b36de4.chunk.js"
+  },
+  {
+    "revision": "e410ac814002d1aefe93",
+    "url": "/homepage/static/js/runtime-main.ae933cb9.js"
+  }
+]);
